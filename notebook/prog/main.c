9708bf70ae4de60e32dfffa50da4e57acdb301fa
Michael Gene Brockus <55331536+troglobyte-coder@users.noreply.github.com>
2022-06-01 16:52:49 -0700

    Create main.c

---
+++ notebook/prog/main.c
@@ -0,0 +1,18 @@
+/*
+ *
+ * Troglobyte Learning:
+ * Teacher: Michael Gene Brockus
+ * Gmail: <mail: michaelbrockus@gmail.com>
+ *
+ */
+#include <stdio.h>
+
+enum { SUCCESS = 0 };
+
+//
+// main is where all programs start
+int main(void)
+{
+    puts("Hello, World");
+    return SUCCESS;
+} // end of func main
